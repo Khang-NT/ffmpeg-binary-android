16c328d69ef5b6da71e20020e5e43bd9a1b7a803
Khang Nguyen <khang.neon.1997@gmail.com>
2017-12-30 14:56:39 +0700

    Update build_all.sh: enable all arch

--- build_scripts/build_all.sh
+++ build_scripts/build_all.sh
@@ -20,29 +20,29 @@ TARGET_MIPS64_DIR=$BUILD_DIR/mips64
 TARGET_X86_64_DIR=$BUILD_DIR/x86_64
 TARGET_ARMEABI_64_DIR=$BUILD_DIR/arm64-v8a
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh i686 $TARGET_X86_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh i686 $TARGET_X86_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh x86_64 $TARGET_X86_64_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh x86_64 $TARGET_X86_64_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh arm $TARGET_ARMEABI_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh arm $TARGET_ARMEABI_DIR
 
 cd $WORKING_DIR
 ./build_ffmpeg.sh armv7-a $TARGET_ARMEABIV7A_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh arm-v7n $TARGET_ARMEABIV7N_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh arm-v7n $TARGET_ARMEABIV7N_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh arm64-v8a $TARGET_ARMEABI_64_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh arm64-v8a $TARGET_ARMEABI_64_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh mips $TARGET_MIPS_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh mips $TARGET_MIPS_DIR
 
-# cd $WORKING_DIR
-# ./build_ffmpeg.sh mips64 $TARGET_MIPS64_DIR
+cd $WORKING_DIR
+./build_ffmpeg.sh mips64 $TARGET_MIPS64_DIR
 
 echo "Build complete."
 exit 0

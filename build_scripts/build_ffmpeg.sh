0b8d9c76075f09046bccf66fe0ac8f63aae89905
Khang Nguyen <khang.neon.1997@gmail.com>
2018-04-09 00:48:39 +0700

    Upgrade FFmpeg version 3.4.2

--- build_scripts/build_ffmpeg.sh
+++ build_scripts/build_ffmpeg.sh
@@ -49,7 +49,7 @@ X86_64_PREBUILT=$NDK/toolchains/x86_64-4.9/prebuilt/$OS
 # MIPS64_CROSS_PREFIX=$MIPS64_PREBUILT/bin/$HOST-
 
 
-FFMPEG_VERSION="3.3.2"
+FFMPEG_VERSION="3.4.2"
 if [ ! -d "ffmpeg-${FFMPEG_VERSION}" ]; then
     echo "Downloading ffmpeg-${FFMPEG_VERSION}.tar.bz2"
     curl -LO http://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2

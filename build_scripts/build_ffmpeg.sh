5c1a50dbedc0786f1a124c0014c9e53439888b1a
Khang Nguyen <khang.neon.1997@gmail.com>
2018-04-08 23:05:16 +0700

    Update build_ffmpeg.sh to ready for CI

--- build_scripts/build_ffmpeg.sh
+++ build_scripts/build_ffmpeg.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
+## $1: target
+## $2: build dir (prefix)
+## $3: destination directory where ffmpeg binary will copy to
+
 set -e
 set -x
 
+## Support either NDK linux or darwin (mac os)
+## Check $NDK exists
+if [ "$NDK" = "" ] || [ ! -d $NDK ]; then
+	echo "NDK variable not set or path to NDK is invalid, exiting..."
+	exit 1
+fi
+
 export TARGET=$1
 export PREFIX=$2
+export DESTINATION_FOLDER=$3
 
-ARM_PLATFORM=$NDK/platforms/android-16/arch-arm/
-ARM_PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+if [ "$(uname)" == "Darwin" ]; then
+    OS="darwin-x86_64"
+else
+    OS="linux-x86_64"
+fi
 
-ARM64_PLATFORM=$NDK/platforms/android-21/arch-arm64/
-ARM64_PREBUILT=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64
+NATIVE_SYSROOT=/
 
-X86_PLATFORM=$NDK/platforms/android-16/arch-x86/
-X86_PREBUILT=$NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64
+ARM_SYSROOT=$NDK/platforms/android-16/arch-arm/
+ARM_PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/$OS
 
-X86_64_PLATFORM=$NDK/platforms/android-21/arch-x86_64/
-X86_64_PREBUILT=$NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64
+ARM64_SYSROOT=$NDK/platforms/android-21/arch-arm64/
+ARM64_PREBUILT=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/$OS
 
-MIPS_PLATFORM=$NDK/platforms/android-16/arch-mips/
-MIPS_PREBUILT=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/darwin-x86_64
+X86_SYSROOT=$NDK/platforms/android-16/arch-x86/
+X86_PREBUILT=$NDK/toolchains/x86-4.9/prebuilt/$OS
 
-MIPS64_PLATFORM=$NDK/platforms/android-21/arch-mips64/
-MIPS64_PREBUILT=$NDK/toolchains/mips64el-linux-android-4.9/prebuilt/darwin-x86_64
+X86_64_SYSROOT=$NDK/platforms/android-21/arch-x86_64/
+X86_64_PREBUILT=$NDK/toolchains/x86_64-4.9/prebuilt/$OS
+
+## No longer support MIPS MIPS64
+
+# MIPS_SYSROOT=$NDK/platforms/android-16/arch-mips/
+# MIPS_PREBUILT=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/darwin-x86_64
+# MIPS_CROSS_PREFIX=$MIPS_PREBUILT/bin/$HOST-
+
+# MIPS64_SYSROOT=$NDK/platforms/android-21/arch-mips64/
+# MIPS64_PREBUILT=$NDK/toolchains/mips64el-linux-android-4.9/prebuilt/darwin-x86_64
+# MIPS64_CROSS_PREFIX=$MIPS64_PREBUILT/bin/$HOST-
 
 
 FFMPEG_VERSION="3.3.2"
@@ -44,12 +68,12 @@ else
     echo "Using existing `pwd`/yasm-${YASM_VERSION}"
 fi
 
-# if [ ! -d "x264" ]; then
-#     echo "Cloning x264"
-#     git clone --depth=1 git://git.videolan.org/x264.git x264
-# else
-#     echo "Using existing `pwd`/x264"
-# fi
+if [ ! -d "x264" ]; then
+    echo "Cloning x264"
+    git clone --depth=1 git://git.videolan.org/x264.git x264
+else
+    echo "Using existing `pwd`/x264"
+fi
 
 OPUS_VERSION="1.1.5"
 if [ ! -d "opus-${OPUS_VERSION}" ]; then
@@ -60,14 +84,14 @@ else
     echo "Using existing `pwd`/opus-${OPUS_VERSION}"
 fi
 
-# FDK_AAC_VERSION="0.1.5"
-# if [ ! -d "fdk-aac-${FDK_AAC_VERSION}" ]; then
-#     echo "Downloading fdk-aac-${FDK_AAC_VERSION}"
-#     curl -LO http://downloads.sourceforge.net/opencore-amr/fdk-aac-${FDK_AAC_VERSION}.tar.gz
-#     tar -xvzf fdk-aac-${FDK_AAC_VERSION}.tar.gz
-# else
-#     echo "Using existing `pwd`/fdk-aac-${FDK_AAC_VERSION}"
-# fi
+FDK_AAC_VERSION="0.1.5"
+if [ ! -d "fdk-aac-${FDK_AAC_VERSION}" ]; then
+    echo "Downloading fdk-aac-${FDK_AAC_VERSION}"
+    curl -LO http://downloads.sourceforge.net/opencore-amr/fdk-aac-${FDK_AAC_VERSION}.tar.gz
+    tar -xvzf fdk-aac-${FDK_AAC_VERSION}.tar.gz
+else
+    echo "Using existing `pwd`/fdk-aac-${FDK_AAC_VERSION}"
+fi
 
 LAME_MAJOR="3.99"
 LAME_VERSION="3.99.5"
@@ -118,37 +142,44 @@ fi
 
 function build_one
 {
-if [ $ARCH == "arm" ]
+if [ $ARCH == "native" ]
 then
-    PLATFORM=$ARM_PLATFORM
+    SYSROOT=$NATIVE_SYSROOT
+    HOST=
+    CROSS_PREFIX=
+elif [ $ARCH == "arm" ]
+then
+    SYSROOT=$ARM_SYSROOT
     HOST=arm-linux-androideabi
-    export CROSS_PREFIX=$ARM_PREBUILT/bin/$HOST-
+    CROSS_PREFIX=$ARM_PREBUILT/bin/$HOST-
 #added by alexvas
 elif [ $ARCH == "arm64" ]
 then
-    PLATFORM=$ARM64_PLATFORM
+    SYSROOT=$ARM64_SYSROOT
     HOST=aarch64-linux-android
-    export CROSS_PREFIX=$ARM64_PREBUILT/bin/$HOST-
-elif [ $ARCH == "mips" ]
-then
-    PLATFORM=$MIPS_PLATFORM
-    HOST=mipsel-linux-android
-    export CROSS_PREFIX=$MIPS_PREBUILT/bin/$HOST-
-elif [ $ARCH == "mips64" ]
-then
-    PLATFORM=$MIPS64_PLATFORM
-    HOST=mips64el-linux-android
-    export CROSS_PREFIX=$MIPS64_PREBUILT/bin/$HOST-
+    CROSS_PREFIX=$ARM64_PREBUILT/bin/$HOST-
 elif [ $ARCH == "x86_64" ]
 then
-    PLATFORM=$X86_64_PLATFORM
+    SYSROOT=$X86_64_SYSROOT
     HOST=x86_64-linux-android
-    export CROSS_PREFIX=$X86_64_PREBUILT/bin/$HOST-
+    CROSS_PREFIX=$X86_64_PREBUILT/bin/$HOST-
 elif [ $ARCH == "i686" ]
 then
-    PLATFORM=$X86_PLATFORM
+    SYSROOT=$X86_SYSROOT
     HOST=i686-linux-android
-    export CROSS_PREFIX=$X86_PREBUILT/bin/$HOST-
+    CROSS_PREFIX=$X86_PREBUILT/bin/$HOST-
+
+# elif [ $ARCH == "mips" ]
+# then
+#     SYSROOT=$MIPS_SYSROOT
+#     HOST=mipsel-linux-android
+#     CROSS_PREFIX=$MIPS_CROSS_PREFIX
+# elif [ $ARCH == "mips64" ]
+# then
+#     SYSROOT=$MIPS64_SYSROOT
+#     HOST=mips64el-linux-android
+#     CROSS_PREFIX=$MIPS64_CROSS_PREFIX
+
 fi
 
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
@@ -160,34 +191,36 @@ export AR="${CROSS_PREFIX}ar"
 export NM="${CROSS_PREFIX}nm"
 export RANLIB="${CROSS_PREFIX}ranlib"
 export LDFLAGS="-L$PREFIX/lib -fPIE -pie "
-export CFLAGS="$OPTIMIZE_CFLAGS -I$PREFIX/include --sysroot=$PLATFORM -fPIE -Dlog2\(x\)=\(log\(x\)/log\(2\)\) -Dlog2f\(x\)=\(logf\(x\)/log\(2\)\) "
+export CFLAGS="$OPTIMIZE_CFLAGS -I$PREFIX/include --sysroot=$SYSROOT -fPIE -Dlog2\(x\)=\(log\(x\)/log\(2\)\) -Dlog2f\(x\)=\(logf\(x\)/log\(2\)\) "
 export CXXFLAGS="$CFLAGS "
-export CPPFLAGS="--sysroot=$PLATFORM "
+export CPPFLAGS="--sysroot=$SYSROOT "
 export STRIP=${CROSS_PREFIX}strip
 
 pushd yasm-${YASM_VERSION}
 ./configure --prefix=$PREFIX \
     --host=$HOST
+
 make clean
 make -j8
 make install
 popd
 
-# pushd x264
-# ./configure \
-#     --cross-prefix=$CROSS_PREFIX \
-#     --sysroot=$PLATFORM \
-#     --host=$HOST \
-#     --enable-pic \
-#     --enable-static \
-#     --disable-shared \
-#     --disable-cli \
-#     --disable-opencl \
-#     --prefix=$PREFIX
-# make clean
-# make -j8
-# make install
-# popd
+pushd x264
+./configure \
+    --cross-prefix=$CROSS_PREFIX \
+    --sysroot=$SYSROOT \
+    --host=$HOST \
+    --enable-pic \
+    --enable-static \
+    --disable-shared \
+    --disable-cli \
+    --disable-opencl \
+    --prefix=$PREFIX
+
+make clean
+make -j8
+make install
+popd
 
 pushd opus-${OPUS_VERSION}
 ./configure \
@@ -205,17 +238,18 @@ popd
 
 
 # Non-free
-# pushd fdk-aac-${FDK_AAC_VERSION}
-# ./configure \
-#     --prefix=$PREFIX \
-#     --host=$HOST \
-#     --enable-static \
-#     --disable-shared \
-#     --with-sysroot=$PLATFORM
-# make clean
-# make -j8
-# make install
-# popd
+pushd fdk-aac-${FDK_AAC_VERSION}
+./configure \
+    --prefix=$PREFIX \
+    --host=$HOST \
+    --enable-static \
+    --disable-shared \
+    --with-sysroot=$SYSROOT
+
+make clean
+make -j8
+make install
+popd
 
 pushd lame-${LAME_VERSION}
 ./configure \
@@ -248,7 +282,8 @@ pushd libogg-${LIBOGG_VERSION}
     --host=$HOST \
     --enable-static \
     --disable-shared \
-    --with-sysroot=$PLATFORM
+    --with-sysroot=$SYSROOT
+
 make clean
 make -j8
 make install
@@ -260,8 +295,9 @@ pushd libvorbis-${LIBVORBIS_VERSION}
     --host=$HOST \
     --enable-static \
     --disable-shared \
-    --with-sysroot=$PLATFORM \
+    --with-sysroot=$SYSROOT \
     --with-ogg=$PREFIX
+
 make clean
 make -j8
 make install
@@ -270,14 +306,58 @@ popd
 # (wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl && \
 #     chmod +x gas-preprocessor.pl && \
 #     sudo mv gas-preprocessor.pl /usr/bin) || exit 1
-
 pushd ffmpeg-$FFMPEG_VERSION
+
+if [ $ARCH == "native" ]
+    CROSS_COMPILE_FLAGS=
+else 
+    CROSS_COMPILE_FLAGS=--target-os=$TARGET_OS \
+        --arch=$ARCH \
+        --cross-prefix=$CROSS_PREFIX \
+        --enable-cross-compile \
+        --sysroot=$SYSROOT \
+fi
+
+# Build - FULL version
 ./configure --prefix=$PREFIX \
-    --target-os=linux \
-    --arch=$ARCH \
-    --cross-prefix=$CROSS_PREFIX \
-    --enable-cross-compile \
-    --sysroot=$PLATFORM \
+    $CROSS_COMPILE_FLAGS \
+    --pkg-config=$(which pkg-config) \
+    --pkg-config-flags="--static" \
+    --enable-pic \
+    --enable-small \
+    --enable-gpl \
+    --enable-nonfree \
+    \
+    --disable-shared \
+    --enable-static \
+    \
+    --enable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    \
+    --enable-libshine \
+    --enable-libmp3lame \
+    --enable-libopus \
+    --enable-libvorbis \
+    --enable-libx264 \
+    --enable-libfdk-aac \
+    --enable-bsf=aac_adtstoasc \
+    \
+    --disable-doc \
+    $ADDITIONAL_CONFIGURE_FLAG
+
+make clean
+make -j8
+make install V=1
+
+mkdir -p $DESTINATION_FOLDER/full/
+cp $PREFIX/bin/ffmpeg $DESTINATION_FOLDER/full/
+
+
+# Build - LITE version
+./configure --prefix=$PREFIX \
+    $CROSS_COMPILE_FLAGS \
     --pkg-config=$(which pkg-config) \
     --pkg-config-flags="--static" \
     --enable-pic \
@@ -317,6 +397,10 @@ pushd ffmpeg-$FFMPEG_VERSION
 make clean
 make -j8
 make install V=1
+
+mkdir -p $DESTINATION_FOLDER/lite/
+cp $PREFIX/bin/ffmpeg $DESTINATION_FOLDER/lite/
+
 popd
 }
 
@@ -401,7 +485,7 @@ elif [ $TARGET == 'mips64' ]; then
     ADDITIONAL_CONFIGURE_FLAG=
     build_one
 elif [ $TARGET == 'armv7-a' ]; then
-    #arm armv7-a
+    # armv7-a
     CPU=armv7-a
     ARCH=arm
     OPTIMIZE_CFLAGS="-mfloat-abi=softfp -marm -march=$CPU -Os -O3 "
@@ -414,5 +498,15 @@ elif [ $TARGET == 'arm' ]; then
     OPTIMIZE_CFLAGS="-march=$CPU -Os -O3 "
     ADDITIONAL_CONFIGURE_FLAG=
     build_one
+elif [ $TARGET == 'native' ]; then
+    # host = current machine
+    CPU=x86-64
+    ARCH=native
+    OPTIMIZE_CFLAGS="-O2 -pipe -march=native"
+    ADDITIONAL_CONFIGURE_FLAG=
+    build_one
+elif
+    echo "Unknown target: $TARGET"
+    exit 1
 fi
 
